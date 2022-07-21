c03500abb1b3296c6cccd953b2a2b779b981cd47
cloudwi <cloudwi@naver.com>
2022-07-21 10:45:17 +0900

    calender add
    Signed-off-by: cloudwi <cloudwi@naver.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,14 @@ import Header from './component/Header'
 import MainBanner from './component/MainBanner'
 import { Routes, Route } from 'react-router-dom'
 import Login from './page/Login'
-<<<<<<< HEAD
+
 import Signup from './page/Signup'
-=======
+
 import Calendar from 'react-calendar'
 import 'react-calendar/dist/Calendar.css'
 import { useState } from 'react'
 import Box from '@mui/material/Box'
->>>>>>> 4151894 (calender add)
+
 function App() {
   const [value, setValue] = useState(new Date())
 
@@ -28,12 +28,11 @@ function App() {
           }
         />
         <Route path="/login" element={<Login></Login>} />
-<<<<<<< HEAD
+
         <Route path="/signup" element={<Signup></Signup>} />
-=======
+
         <Route path="/match" element={<div></div>} />
         <Route path="*" element={<div></div>} />
->>>>>>> 4151894 (calender add)
       </Routes>
     </div>
   )
