9354e4489a3ac64c9de9925b43fc79ae357200d4
cloudwi <cloudwi@naver.com>
2022-07-21 10:43:00 +0900

    test
    Signed-off-by: cloudwi <cloudwi@naver.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,23 +2,38 @@ import Header from './component/Header'
 import MainBanner from './component/MainBanner'
 import { Routes, Route } from 'react-router-dom'
 import Login from './page/Login'
+<<<<<<< HEAD
 import Signup from './page/Signup'
+=======
+import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css'
+import { useState } from 'react'
+import Box from '@mui/material/Box'
+>>>>>>> 4151894 (calender add)
 function App() {
+  const [value, setValue] = useState(new Date())
+
   return (
     <div>
       <Header></Header>
-
       <Routes>
         <Route
           path="/"
           element={
             <>
               <MainBanner></MainBanner>
+              <Box sx={{ m: 5 }}></Box>
+              <Calendar onChange={setValue} value={value}></Calendar>
             </>
           }
         />
         <Route path="/login" element={<Login></Login>} />
+<<<<<<< HEAD
         <Route path="/signup" element={<Signup></Signup>} />
+=======
+        <Route path="/match" element={<div></div>} />
+        <Route path="*" element={<div></div>} />
+>>>>>>> 4151894 (calender add)
       </Routes>
     </div>
   )
--- frontend/src/component/MainBanner.js
+++ frontend/src/component/MainBanner.js
@@ -3,22 +3,28 @@ import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import { data } from '../data'
 import Slider from 'react-slick'
+import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
 
 const MainBanner = () => {
   const settings = {
+    customPaging: function (i) {
+      return <div>{i + 1}</div>
+    },
     dots: true,
+    dotsClass: 'slick-dots slick-thumb',
     infinite: true,
+    speed: 500,
+    autoplaySpeed: 5000,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: true,
-    speed: 2000,
-    autoplaySpeed: 5000,
   }
+
   return (
     <div>
       <Slider {...settings}>
-        {data.map((image) => (
-          <Img url={image.image}></Img>
+        {data.map((image, index) => (
+          <Img key={index} url={image.image}></Img>
         ))}
       </Slider>
     </div>
@@ -29,7 +35,8 @@ export default MainBanner
 
 const Img = styled.img`
   width: 100%;
-  height: 500px;
+  height: 400px;
+  border: 0;
   background-image: url(${(props) => props.url});
   background-repeat: no-repeat;
   background-position: center;
