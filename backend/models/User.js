e2653eaf26e5df74e9e3f2d0d81bacafe76d16a8
cloudwi <cloudwi@naver.com>
2022-07-31 10:41:15 +0900

    비밀전보 보호
    Signed-off-by: cloudwi <cloudwi@naver.com>

--- backend/models/User.js
+++ backend/models/User.js
@@ -8,8 +8,6 @@ const userSchema = mongoose.Schema({
   email: {
     type: String,
     trim: true,
-
-    unique: 1,
   },
   password: {
     type: String,
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,21 @@
 const express = require('express')
 const app = express()
 const port = 8080
+const bodyParser = require('body-parser')
+const { User } = require('./models/User')
+
+const config = require('./config/key')
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+//application/json
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
 
 mongoose
-  .connect(
-    'mongodb+srv://cloudwi:MN77868@box.rp8qn.mongodb.net/?retryWrites=true&w=majority'
-  )
+  .connect(config.mongoURI)
   .then(() => {
     console.log('MongoDB Connected')
   })
@@ -15,5 +23,17 @@ mongoose
     console.log(error)
   })
 
-app.get('/', (req, res) => res.send('Hello World'))
+app.get('/', (req, res) => res.send('Hello World 안녕하세요! 바꾸어본다'))
+
+app.post('/signup', (req, res) => {
+  const user = new User(req.body)
+
+  user.save((error, userInfo) => {
+    if (error) return res.json({ success: false, error })
+    return res.status(200).json({
+      success: true,
+    })
+  })
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
